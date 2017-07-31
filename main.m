bcedaf5bed50b0a434fc4885e341173eb75c9f4e
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-07-31 16:53:36 -0400

    Update main.m

--- main.m
+++ main.m
@@ -5,10 +5,12 @@ case 'IUHPC'
 	disp('loading paths (HPC)')
 	addpath(genpath('/N/u/hayashis/BigRed2/git/jsonlab'))
 	addpath(genpath('/N/u/hayashis/BigRed2/git/afq'))
+	addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
 case 'VM'
 	disp('loading paths (VM)')
 	addpath(genpath('/usr/local/jsonlab'))
 	addpath(genpath('/usr/local/afq'))
+	addpath(genpath('/usr/local/vistasoft'))
 end
 
 % load config.json
