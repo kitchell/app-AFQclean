7e975200edbf34d5001b9e85c320a07a59b935c3
kitchell <lindseykitchell21@gmail.com>
2017-07-31 14:35:42 -0400

    add files

---
+++ main.m
@@ -0,0 +1,54 @@
+function main()
+
+switch getenv('ENV')
+case 'IUHPC'
+	disp('loading paths (HPC)')
+	addpath(genpath('/N/u/hayashis/BigRed2/git/jsonlab'))
+	addpath(genpath('/N/u/hayashis/BigRed2/git/afq'))
+case 'VM'
+	disp('loading paths (VM)')
+	addpath(genpath('/usr/local/jsonlab'))
+	addpath(genpath('/usr/local/afq'))
+end
+
+% load config.json
+config = loadjson('config.json');
+
+disp('config dump')
+disp(config)
+
+load(config.afq_fg);
+
+fg_classified = AFQ_clean(fg_classified);
+
+
+save('output.mat', 'fg_classified', 'classification', '-v7.3');
+
+
+mkdir('tracts');
+
+% Make colors for the tracts
+cm = parula(length(tracts));
+for it = 1:length(tracts)
+   tract.name   = tracts(it).name;
+   tract.color  = cm(it,:);
+   tract.coords = tracts(it).fibers;
+   savejson('', tract, fullfile('tracts',sprintf('%i.json',it)));
+   clear tract
+end
+
+% saving text file with number of fibers per tracts
+tract_info = cell(length(fg_classified), 2);
+
+for i = 1:length(fg_classified)
+    tract_info{i,1} = fg_classified(i).name;
+    tract_info{i,2} = length(fg_classified(i).fibers);
+end
+
+T = cell2table(tract_info);
+T.Properties.VariableNames = {'Tracts', 'FiberCount'};
+
+writetable(T,'output_fibercounts.txt')
+
+end
+
