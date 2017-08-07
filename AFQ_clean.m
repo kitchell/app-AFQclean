8c069d090359bdf09f4655ff6f4e6803aa363cc9
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-08-07 09:25:14 -0400

    Update AFQ_clean.m

--- AFQ_clean.m
+++ AFQ_clean.m
@@ -1,19 +1,23 @@
-function fg_clean = AFQ_clean(fg_classified)
+function fg_clean = AFQ_clean(fg_classified, config)
 
 % cleans the fiber tracts with AFQ_removeFiberOutliers
 
-maxDist = 4;
+#maxDist = 4;
+maxDist = config.maxdist;
 
-maxLen = 4;
+#maxLen = 4;
+maxLen = config.maxlen;
 
-numNodes = 30;
+#numNodes = 30;
+numNodes = config.numnodes;
 
-M = 'mean';
+#M = 'mean';
+M = config.M;
 
-maxIter = 1;
+#maxIter = 5;
+maxIter = config.maxiter;
 
 count = true;
-
 
 for ii = 1:length(fg_classified)
     fg_clean(ii) = AFQ_removeFiberOutliers(fg_classified(ii), maxDist, maxLen, numNodes, M, count, maxIter);
