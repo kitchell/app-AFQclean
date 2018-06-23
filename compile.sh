c2eb56d54cd37f088b9c25ccc2787c62cd6ba1ff
Soichi Hayashi <hayashis@iu.edu>
2018-06-22 20:48:54 -0400

    added compile.sh

---
+++ compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+module load matlab/2017a
+
+log=compiled/commit_ids.txt
+true > $log
+echo "/N/u/brlife/git/vistasoft" >> $log
+(cd /N/u/brlife/git/vistasoft && git log -1) >> $log
+echo "/N/u/brlife/git/jsonlab" >> $log
+(cd /N/u/brlife/git/jsonlab && git log -1) >> $log
+echo "/N/u/brlife/git/afq" >> $log
+(cd /N/u/brlife/git/afq && git log -1) >> $log
+
+cat > build.m <<END
+addpath(genpath('/N/u/brlife/git/jsonlab'))
+addpath(genpath('/N/u/brlife/git/afq'))
+addpath(genpath('/N/u/brlife/git/vistasoft'))
+
+mcc -m -R -nodisplay -d compiled afqcleantracts
+exit
+END
+matlab -nodisplay -nosplash -r build && rm build.m
